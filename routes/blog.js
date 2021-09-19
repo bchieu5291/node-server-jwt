52c72885a55f16f28a7520025dc170fc332ac538
georgehill5291 <hieuden0@gmail.com>
2021-09-19 22:19:57 +0700

    init public blog

--- routes/blog.js
+++ routes/blog.js
@@ -1,31 +1,42 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../middleware/auth");
+const express = require('express')
+const router = express.Router()
+const verifyToken = require('../middleware/auth')
 
-const Blog = require("../models/blog");
+const Blog = require('../models/blog')
 
 //@GET
 //@access private
-router.get("/", verifyToken, async (req, res) => {
+router.get('/', verifyToken, async (req, res) => {
     try {
         const blogs = await Blog.find({ user: req.userId })
-            .sort({ createAt: "desc" })
-            .populate("user", ["username"]);
+            .sort({ createAt: 'desc' })
+            .populate('user', ['username'])
 
-        res.json({ success: true, blogs: blogs });
+        res.json({ success: true, blogs: blogs })
     } catch (error) {
-        console.log(error);
-        return res.status(400).json({ success: false, message: "General error" });
+        console.log(error)
+        return res.status(400).json({ success: false, message: 'General error' })
     }
-});
+})
+
+router.get('/public', async (req, res) => {
+    try {
+        const blogs = await Blog.find().sort({ createAt: 'desc' }).populate('user', ['username'])
+
+        res.json({ success: true, blogs: blogs })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ success: false, message: 'General error' })
+    }
+})
 
 //@BLOG
 //@access private
-router.post("/", verifyToken, async (req, res) => {
-    const { title, description } = req.body;
+router.post('/', verifyToken, async (req, res) => {
+    const { title, description } = req.body
 
     if (!title) {
-        return res.status(400).json({ success: false, message: "Title is required " });
+        return res.status(400).json({ success: false, message: 'Title is required ' })
     }
 
     try {
@@ -33,94 +44,94 @@ router.post("/", verifyToken, async (req, res) => {
             title,
             description,
             user: req.userId,
-        });
+        })
 
-        await newBlog.save();
+        await newBlog.save()
 
-        res.json({ success: true, message: "Successs", blog: newBlog });
+        res.json({ success: true, message: 'Successs', blog: newBlog })
     } catch (error) {
-        console.log(error);
-        return res.status(400).json({ success: false, message: "General error" });
+        console.log(error)
+        return res.status(400).json({ success: false, message: 'General error' })
     }
-});
+})
 
 //@PUT
 //@access private
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
     try {
-        const findBlogCondition = { _id: req.params.id };
+        const findBlogCondition = { _id: req.params.id }
 
-        const existBlog = await Blog.findOne(findBlogCondition);
+        const existBlog = await Blog.findOne(findBlogCondition)
 
         //user not author to update blog
         if (!existBlog) {
             return res.status(401).json({
                 success: false,
-                message: "Blog not found",
-            });
+                message: 'Blog not found',
+            })
         }
 
-        res.json({ success: true, message: "Successs", blog: existBlog });
+        res.json({ success: true, message: 'Successs', blog: existBlog })
     } catch (error) {
-        console.log(error);
-        return res.status(400).json({ success: false, message: "General error" });
+        console.log(error)
+        return res.status(400).json({ success: false, message: 'General error' })
     }
-});
+})
 
 //@PUT
 //@access private
-router.put("/:id", verifyToken, async (req, res) => {
-    const { title, description, url, status } = req.body;
+router.put('/:id', verifyToken, async (req, res) => {
+    const { title, description, url, status } = req.body
 
     if (!title) {
-        return res.status(400).json({ success: false, message: "Title is required" });
+        return res.status(400).json({ success: false, message: 'Title is required' })
     }
 
     try {
         let updatedBlog = {
             title,
-            description: description || "",
-        };
+            description: description || '',
+        }
 
-        const blogUpdateCondition = { _id: req.params.id, user: req.userId };
+        const blogUpdateCondition = { _id: req.params.id, user: req.userId }
 
-        updatedBlog = await Blog.findOneAndUpdate(blogUpdateCondition, updatedBlog, { new: true });
+        updatedBlog = await Blog.findOneAndUpdate(blogUpdateCondition, updatedBlog, { new: true })
 
         //user not author to update blog
         if (!updatedBlog) {
             return res
                 .status(401)
-                .json({ success: false, message: "Blog not found || not authorize" });
+                .json({ success: false, message: 'Blog not found || not authorize' })
         }
 
-        res.json({ success: true, message: "Successs", blog: updatedBlog });
+        res.json({ success: true, message: 'Successs', blog: updatedBlog })
     } catch (error) {
-        console.log(error);
-        return res.status(400).json({ success: false, message: "General error" });
+        console.log(error)
+        return res.status(400).json({ success: false, message: 'General error' })
     }
-});
+})
 
 //@BLOG
 //@access private
-router.delete("/:id", verifyToken, async (req, res) => {
+router.delete('/:id', verifyToken, async (req, res) => {
     try {
-        const blogdDeleteCondition = { _id: req.params.id, user: req.userId };
+        const blogdDeleteCondition = { _id: req.params.id, user: req.userId }
 
-        const deletedBlog = await Blog.findOneAndDelete(blogdDeleteCondition);
+        const deletedBlog = await Blog.findOneAndDelete(blogdDeleteCondition)
 
         //user not author to update blog
         if (!deletedBlog) {
             return res.status(401).json({
                 success: false,
-                message: "Blog not found || not authorize",
-            });
+                message: 'Blog not found || not authorize',
+            })
         }
 
-        res.json({ success: true, message: "Successs", blog: deletedBlog });
+        res.json({ success: true, message: 'Successs', blog: deletedBlog })
     } catch (error) {
-        console.log(error);
-        return res.status(400).json({ success: false, message: "General error" });
+        console.log(error)
+        return res.status(400).json({ success: false, message: 'General error' })
     }
-});
+})
 
-module.exports = router;
+module.exports = router
